6650909a7710e9559214a3a9ec181edda23a0dbd
Richard S. Graham <RichGraham@users.noreply.github.com>
2022-06-22 16:26:35 +0100

    Attempt to make more platform independent

--- basic.py
+++ basic.py
@@ -1,7 +1,15 @@
-import multiprocess as mp  
-import time  
 import pyDOE
 import numpy as np
+import importlib
+
+multiprocess_spec = importlib.util.find_spec("multiprocess")
+foundMultiprocess =  multiprocess_spec is not None
+
+if( foundMultiprocess ):
+    import multiprocess as mp    
+else:
+    import multiprocessor as mp    
+
 
 class someParams:
     def __init__(self, x,y,initialSeed):
@@ -16,10 +24,13 @@ class someParams:
         myLHC =  pyDOE.lhs(x,samples=y)
         return sum([sum(i) for i in myLHC])/x/y-0.5
 
+number_of_Processors = mp.cpu_count()
 
+print('Found '+str(number_of_Processors)+' processors')
+    
 myClass = someParams( 5,5,2)
-myPool = mp.Pool(4)  
-results = myPool.map(myClass.getLHC, [i for i in range(4)])  
+myPool = mp.Pool( number_of_Processors )  
+results = myPool.map(myClass.getLHC, [i for i in range(number_of_Processors)])  
 myPool.close()  
 myPool.join()
 print(results)
@@ -27,38 +38,38 @@ print(results)
 
 
 myClass = someParams( 5,5,2)
-myPool = mp.Pool(4)  
-results = myPool.map(myClass.getLHC, [i for i in range(4)])  
+myPool = mp.Pool( number_of_Processors)  
+results = myPool.map(myClass.getLHC, [i for i in range(number_of_Processors)])  
 myPool.close()  
 myPool.join()
 print(results)
 
 
 myClass = someParams( 5,5,3)
-myPool = mp.Pool(4)  
-results = myPool.map(myClass.getLHC, [i for i in range(4)])  
+myPool = mp.Pool(number_of_Processors)  
+results = myPool.map(myClass.getLHC, [i for i in range(number_of_Processors)])  
 myPool.close()  
 myPool.join()
 print(results)
 
 myClass = someParams( 5,5,3)
-myPool = mp.Pool(4)  
-results = myPool.map(myClass.getLHC, [i for i in range(4)])  
+myPool = mp.Pool(number_of_Processors)  
+results = myPool.map(myClass.getLHC, [i for i in range(number_of_Processors)])  
 myPool.close()  
 myPool.join()
 print(results)
 
 myClass = someParams( 5,5,2)
-myPool = mp.Pool(4)  
-results = myPool.map(myClass.getLHC, [i for i in range(4)])  
+myPool = mp.Pool(number_of_Processors)  
+results = myPool.map(myClass.getLHC, [i for i in range(number_of_Processors)])  
 myPool.close()  
 myPool.join()
 print(results)
 
 
-myClass = someParams( 5000,5000,3)
-myPool = mp.Pool(4)  
-results = myPool.map(myClass.getLHC, [i for i in range(4)])  
+myClass = someParams( 1000,1000,3)
+myPool = mp.Pool(number_of_Processors)  
+results = myPool.map(myClass.getLHC, [i for i in range(number_of_Processors)])  
 myPool.close()  
 myPool.join()
 print(results)
