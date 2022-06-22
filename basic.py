fe50d81bb7ccfd9c7c10e9915097f584821e0eb8
Richard S. Graham <RichGraham@users.noreply.github.com>
2022-06-22 16:51:51 +0100

    Added some stuff to allow for timing

--- basic.py
+++ basic.py
@@ -1,6 +1,7 @@
 import pyDOE
 import numpy as np
 import importlib
+import sys
 
 multiprocess_spec = importlib.util.find_spec("multiprocess")
 foundMultiprocess =  multiprocess_spec is not None
@@ -24,50 +25,10 @@ class someParams:
         myLHC =  pyDOE.lhs(x,samples=y)
         return sum([sum(i) for i in myLHC])/x/y-0.5
 
-number_of_Processors = mp.cpu_count()
-
-print('Found '+str(number_of_Processors)+' processors')
-    
-myClass = someParams( 5,5,2)
-myPool = mp.Pool( number_of_Processors )  
-results = myPool.map(myClass.getLHC, [i for i in range(number_of_Processors)])  
-myPool.close()  
-myPool.join()
-print(results)
+number_of_Processors = int(sys.argv[1])
 
 
-
-myClass = someParams( 5,5,2)
-myPool = mp.Pool( number_of_Processors)  
-results = myPool.map(myClass.getLHC, [i for i in range(number_of_Processors)])  
-myPool.close()  
-myPool.join()
-print(results)
-
-
-myClass = someParams( 5,5,3)
-myPool = mp.Pool(number_of_Processors)  
-results = myPool.map(myClass.getLHC, [i for i in range(number_of_Processors)])  
-myPool.close()  
-myPool.join()
-print(results)
-
-myClass = someParams( 5,5,3)
-myPool = mp.Pool(number_of_Processors)  
-results = myPool.map(myClass.getLHC, [i for i in range(number_of_Processors)])  
-myPool.close()  
-myPool.join()
-print(results)
-
-myClass = someParams( 5,5,2)
-myPool = mp.Pool(number_of_Processors)  
-results = myPool.map(myClass.getLHC, [i for i in range(number_of_Processors)])  
-myPool.close()  
-myPool.join()
-print(results)
-
-
-myClass = someParams( 1000,1000,3)
+myClass = someParams( 6000,6000,3)
 myPool = mp.Pool(number_of_Processors)  
 results = myPool.map(myClass.getLHC, [i for i in range(number_of_Processors)])  
 myPool.close()  
