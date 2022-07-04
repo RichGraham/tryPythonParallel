d29c1fe44f071fa81c24f38a66b6084366ac770d
Richard S. Graham <RichGraham@users.noreply.github.com>
2022-07-04 19:30:35 +0100

    Made each run equal length

--- joblib/withFunction.py
+++ joblib/withFunction.py
@@ -3,24 +3,24 @@ import numpy as np
 from math import sqrt
 from joblib import Parallel, delayed
 
-task = 300000
+task = 500000
 
 def myFunction( x ):
     sum = 0
-    for i in range(task * x):
-        sum += np.exp( pow(i,1/task))
+    for i in range(task ):
+        sum += np.exp( pow(x,1.5*i/task))
     return sum
 
 
 size = 12
 
 t = time.time()
-results= Parallel(n_jobs=3)(delayed(myFunction)(i ) for i in range(size))
+results= Parallel(n_jobs=1)(delayed(myFunction)(1.1 ) for i in range(size))
 print(time.time() - t)
 print(results)
 
 t = time.time()
-results= Parallel(n_jobs=4)(delayed(myFunction)(i ) for i in range(size))
+results= Parallel(n_jobs=2)(delayed(myFunction)( 1.1 ) for i in range(size))
 print(time.time() - t)
 print(results)
 
